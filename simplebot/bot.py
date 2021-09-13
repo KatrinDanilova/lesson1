f6846b607439a88ab6d01804e4981d75346a91d1
Katrin Danilova <danilova.k89@gmail.com>
2021-09-13 17:44:39 +0500

    Changes on telegram bot

--- simplebot/bot.py
+++ simplebot/bot.py
@@ -1,7 +1,16 @@
 #from telegram.ext import Updater
 #from telegram.ext import CommandHandler
+
+import logging, os
 from telegram import Update
-from telegram.ext import Updater, CallbackContext, CommandHandler
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
+
+path = 'd:\Python\learn1\simplebot'
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    filename=os.path.join(path, 'bot.log')
+)
 
 def start_bot(update: Update, context: CallbackContext):
     myuser = update.message.chat.first_name
@@ -10,16 +19,24 @@ def start_bot(update: Update, context: CallbackContext):
 Я простой бот и понимаю только команду /start
     """
     update.message.reply_text(mytext)
-    print(update)
+    #print(update) #список полей update
+
+def chat(update: Update, context: CallbackContext):
+    mytext = update.message.text
+    logging.info(mytext)
+    update.message.reply_text(mytext)
 
 
 def main():
     updtr = Updater('1962228157:AAFEnQahitcgf3WyttR3ZpdUiBLWLQtU0WY', use_context=True)
     updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+    updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+
     updtr.start_polling()
     updtr.idle()
 
 
 if __name__ == "__main__":
+    logging.info('Bot started...')
     main()
 
